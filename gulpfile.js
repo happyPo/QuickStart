bafa791a823bca99a3c31ec1fe39e96247edab5d
braitsev <braitsev.eugene@gmail.com>
2016-09-27 16:26:41 +0300

    add gulp-uglify, gulp-combine-mq

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,29 @@
 'use strict';
 var gulp = require('gulp');
+var rename = require('gulp-rename');
 var cleanCSS = require('gulp-clean-css');
 var notify = require("gulp-notify");
 var autoprefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+var combineMq = require('gulp-combine-mq');
 
 //less
-gulp.task('default', function () {
+gulp.task('cssmin', function () {
     return gulp.src('./css/style.css')
         .pipe(autoprefixer({
             browsers: ['last 3 versions']
         }))
+        .pipe(combineMq({
+            beautify: false
+        }))
         .pipe(cleanCSS({advanced: true}))
         .pipe(gulp.dest('css/'))
         .pipe(notify('Done!'));
+});
+
+gulp.task('jsmin', function () {
+    gulp.src('js/*.js')
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('js'))
 });
---
+++ readme.fed.txt
@@ -0,0 +1,23 @@
+//Pluging
+custom select -  chosen https://harvesthq.github.io/chosen/
+galleries -      owl.carousel http://owlcarousel.owlgraphic.com/
+infographic thumbnails - https://github.com/gijsroge/OwlCarousel2-Thumbs
+popups - bpopup https://github.com/dinbror/bpopup
+tabs - ui tabs
+
+//arguments for phpstorm watcher
+--strict-math=on --source-map=../css/$FileName$.map $FileName$
+
+//gulp modules
+npm i --save-dev
+
+*** theme location: httpdocs\themes\basic
+
+*** static pages: httpdocs\sites\all\themes\samples
+
+*** intranet: https://intranet.bluefountainmedia.com/projects.php?id=3050
+
+FED: Eugene Braitsev
+PM:
+BED:
+QA:
